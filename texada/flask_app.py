6190b3d362d9a02531909598cabe8370fb08572e
Xintong Wang <xintong.wang@mail.mcgill.ca>
2018-07-20 14:31:09 -0400

    create adding record api

--- texada/flask_app.py
+++ texada/flask_app.py
@@ -3,6 +3,7 @@ from flask import request
 from flask import jsonify
 from db import session_scope
 from models import Product
+from models import LocationRecord as Record
 from errors import ClientError
 
 texada_api = Flask("texada_api")
@@ -40,6 +41,34 @@ def get_product(pid):
                     }
 	return jsonify(response)
 
+
+@texada_api.route("/location_records/<pid>", methods=["POST"])
+def add_recrod(pid):
+    keys = ["datetime","longitude","latitude","elevation"]
+    args = request.get_json()
+    print "******DEBUG*******:",args
+    for k in keys:
+        if k not in args:
+            err = "Please provide {} argument".format(k)
+            raise ClientError(err)     
+    with session_scope() as session:
+        product = session.query(
+            Product
+        ).filter(
+            Product.id == pid
+        ).one_or_none()
+        if not product:
+            raise ClientError("Oops! Product not found")
+        entry = Record(
+                    product_id = pid,
+                    datetime = args["datetime"],
+                    longitude = args["longitude"],
+                    latitude = args["latitude"],
+                    elevation = args["elevation"],    
+        )
+        session.add(entry)
+        session.flush()
+    return "Successfully insert record ", args
 
 
 

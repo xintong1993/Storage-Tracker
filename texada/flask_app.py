91183dba894d6bc520c0d2abb7f8414b264d537f
Xintong Wang <xintong.wang@mail.mcgill.ca>
2018-07-21 03:22:29 -0400

    update and delete api

--- texada/flask_app.py
+++ texada/flask_app.py
@@ -20,25 +20,32 @@ def root():
 	return "<h1>Welcome to Xintong's project!/</h1>"
 
 
-@texada_api.route("/products/<pid>", methods=["GET"])
+@texada_api.route("/products/<int:pid>", methods=["GET"])
 def get_product(pid):
-	with session_scope() as session:
-		product = session.query(
-			Product
-		).filter(
-			Product.id == pid
-		).one_or_none()
+    with session_scope() as session:
+        product = session.query(
+            Product
+        ).filter(
+            Product.id == pid
+        ).one_or_none()
+        
+        if not product:
+            raise ClientError("Oops! Product not found")
+    
+        records = session.query(
+            Record
+        ).filter(
+            Record.product_id == pid
+        ).all()
 		
-		if not product:
-			raise ClientError("Oops! Product not found. Please enter a different id.")		
-
-		response = {"description": product.description,
-					"id": product.id,
-		#			"datetime": product.datetime,
-		#			"longitude": product.longitude,
-		#			"latitude": product.latitude,
-		#			"elevation": product.elevation,
-                    }
+        response = {"description": product.description,
+                    "records":[{
+                        "id": record.id,
+                        "datetime": record.datetime,
+                        "longitude": record.longitude,
+                        "latitude": record.latitude,
+                        "elevation": record.elevation,
+                    } for record in records]}
 	return jsonify(response)
 
 
@@ -49,7 +56,7 @@ def add_recrod():
     print "******DEBUG*******:",args
     for k in keys:
         if k not in args:
-            err = "Please provide {} argument".format(k)
+            err = "missing argument {}".format(k)
             raise ClientError(err)     
     with session_scope() as session:
         product = session.query(
@@ -61,18 +68,44 @@ def add_recrod():
             p = Product(description = args["description"])
             session.add(p)
             session.flush()
+            pid = p.id
+        else:
+            pid = product.id
 
-        entry = Record(
-                    product_id = p.id,
+        record = Record(
+                    product_id = pid,
                     datetime = args["datetime"],
                     longitude = args["longitude"],
                     latitude = args["latitude"],
                     elevation = args["elevation"],    
         )
-        session.add(entry)
+        session.add(record)
         session.flush()
-    return "Successfully insert record ", args
+        response = {"record_id": record.id}
+    return jsonify(response)
 
+
+@texada_api.route("/location_records/<int:record_id>", methods=["DELETE","PUT"])
+def modify_record(record_id):
+    args = request.get_json()
+    with session_scope() as session:
+        record = session.query(
+            Record
+        ).filter(
+            Record.id == record_id
+        ).one_or_none()
+        if not record:  
+            raise ClientError("Oops! Record not found")
+        if request.method == "DELETE":
+            session.delete(record)
+            session.flush()
+            response = "successfully deleted record {}".format(record_id)
+        if request.method == "PUT":
+            record.longitude = args.get("longitude", record.longitude)
+            record.latitude = args.get("latitude",record.latitude)
+            record.elevation = args.get("elevation", record.elevation)
+            response = "successfully updated record {}".format(record_id)
+    return response
 
 
 if __name__ == '__main__':

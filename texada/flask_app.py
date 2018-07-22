fc64142a05295821121bbbccdc8ebd3aa4530a79
Xintong Wang <xintong.wang@mail.mcgill.ca>
2018-07-21 20:46:16 -0400

    add datetime in update_record api

--- texada/flask_app.py
+++ texada/flask_app.py
@@ -15,7 +15,7 @@ import datetime
 PAGE_START = 1
 PAGE_LIMIT = 10
 
-texada_api = Flask("texada_api")
+texada_api = Flask('texada_api')
 
 @parser.error_handler
 def handle_args_error(err, request, con):
@@ -29,27 +29,27 @@ def handle_client_error(error):
     return response
 
 
-@texada_api.route("/")
+@texada_api.route('/')
 def root():
 	return "<h1>Welcome to Xintong's project!/</h1>"
 
 
-@texada_api.route("/products", methods=["GET"])
+@texada_api.route('/products', methods=['GET'])
 @use_args({})
 def get_all_products(args):
     with session_scope() as session:
         products = session.query(
             Product
         ).all()
-        response = {"products": [{"description": product.description, 
-                                  "product_id": product.id}
+        response = {'products': [{'description': product.description, 
+                                  'product_id': product.id}
                                 for product in products]
         }
         return jsonify(response)
        
 
 #implement pagination for product 
-@texada_api.route("/products/page", methods=["GET"])
+@texada_api.route('/products/page', methods=['GET'])
 @use_args({
     'start': fields.Int(default = PAGE_START),
     'limit':fields.Int(default = PAGE_LIMIT)
@@ -69,7 +69,7 @@ def get_paginated_list(klass, url, start, limit):
         results = session.query(klass).all()
         count = len(results)
         if (count < start):
-            raise ClientError("Page not found")
+            raise ClientError('Page not found')
         # make response
         obj = {}
         obj['start'] = start
@@ -92,14 +92,14 @@ def get_paginated_list(klass, url, start, limit):
         # finally extract result according to bounds
         res_this_page = results[(start - 1):(start - 1 + limit)]
         obj['results'] =[{
-            "id": product.id,
-            "description": product.description,
+            'id': product.id,
+            'description': product.description,
         } for product in res_this_page]
         return obj
 
 
 
-@texada_api.route("/products/<int:pid>", methods=["GET"])
+@texada_api.route('/products/<int:pid>', methods=['GET'])
 def get_product_by_id(pid):
     with session_scope() as session:
         product = session.query(
@@ -109,7 +109,7 @@ def get_product_by_id(pid):
         ).one_or_none()
         
         if not product:
-            raise ClientError("Oops! Product not found")
+            raise ClientError('Oops! Product not found')
     
         records = session.query(
             Record
@@ -117,44 +117,44 @@ def get_product_by_id(pid):
             Record.product_id == pid
         ).all()
 		
-        response = {"product_id": pid,
-                    "description": product.description,
-                    "records":[{
-                        "record_id": record.id,
-                        "datetime": record.datetime,
-                        "longitude": record.longitude,
-                        "latitude": record.latitude,
-                        "elevation": record.elevation,
+        response = {'product_id': pid,
+                    'description': product.description,
+                    'records':[{
+                        'record_id': record.id,
+                        'datetime': record.datetime,
+                        'longitude': record.longitude,
+                        'latitude': record.latitude,
+                        'elevation': record.elevation,
                     } for record in records]}
 	return jsonify(response)
 
 
-@texada_api.route("/location_records", methods=["POST"])
+@texada_api.route('/location_records', methods=['POST'])
 @use_args({
-    "longitude": fields.Float(required=True),
-    "latitude": fields.Float(required=True),
-    "description": fields.Str(required=True),
-    "datetime": fields.Str(required=True),
-    "elevation": fields.Float(required=True),
+    'longitude': fields.Float(required=True),
+    'latitude': fields.Float(required=True),
+    'description': fields.Str(required=True),
+    'datetime': fields.Str(required=True),
+    'elevation': fields.Float(required=True),
 })
 def add_recrod(args):
-    dt = args["datetime"]
+    dt = args['datetime']
     try:
         start = dt[:19]
         end = dt[19:]
-        datetime.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S")
-        datetime.datetime.strptime(end, "-%H:%M")
+        datetime.datetime.strptime(start, '%Y-%m-%dT%H:%M:%S')
+        datetime.datetime.strptime(end, '-%H:%M')
     except ValueError:
-        raise ClientError("Invalid datetime format, should be yyyy-mm-ddTHH:MM:SS-HH:MM")
+        raise ClientError('Invalid datetime format, should be yyyy-mm-ddTHH:MM:SS-HH:MM')
 
     with session_scope() as session:
         product = session.query(
             Product
         ).filter(
-            Product.description == args["description"]
+            Product.description == args['description']
         ).one_or_none()
         if not product:
-            p = Product(description = args["description"])
+            p = Product(description = args['description'])
             session.add(p)
             session.flush()
             pid = p.id
@@ -163,22 +163,23 @@ def add_recrod(args):
 
         record = Record(
                     product_id = pid,
-                    datetime = args["datetime"],
-                    longitude = args["longitude"],
-                    latitude = args["latitude"],
-                    elevation = args["elevation"],    
+                    datetime = args['datetime'],
+                    longitude = args['longitude'],
+                    latitude = args['latitude'],
+                    elevation = args['elevation'],    
         )
         session.add(record)
         session.flush()
-        response = {"record_id": record.id}
+        response = {'record_id': record.id}
     return jsonify(response)
 
 
-@texada_api.route("/location_records/<int:record_id>", methods=["DELETE","PUT"])
+@texada_api.route('/location_records/<int:record_id>', methods=['DELETE','PUT'])
 @use_args({
-    "longitude": fields.Float(),
-    "latitude": fields.Float(),
-    "elevation": fields.Float(),
+    'longitude': fields.Float(),
+    'latitude': fields.Float(),
+    'elevation': fields.Float(),
+    'datetime': fields.Str(),
 })
 
 def modify_record(args, record_id):
@@ -189,16 +190,26 @@ def modify_record(args, record_id):
             Record.id == record_id
         ).one_or_none()
         if not record:  
-            raise ClientError("Oops! Record not found")
-        if request.method == "DELETE":
+            raise ClientError('Oops! Record not found')
+        if request.method == 'DELETE':
             session.delete(record)
-            session.flush()
-            response = "successfully deleted record {}".format(record_id)
-        if request.method == "PUT":
-            record.longitude = args.get("longitude", record.longitude)
-            record.latitude = args.get("latitude",record.latitude)
-            record.elevation = args.get("elevation", record.elevation)
-            response = "successfully updated record {}".format(record_id)
+            response = 'successfully deleted record {}'.format(record_id)
+        if request.method == 'PUT':
+            #validate datime
+            if args.get('datetime', None):
+                dt = args['datetime']
+                try:
+                    start = dt[:19]
+                    end = dt[19:]
+                    datetime.datetime.strptime(start, '%Y-%m-%dT%H:%M:%S')
+                    datetime.datetime.strptime(end, '-%H:%M')
+                except ValueError:
+                    raise ClientError('Invalid datetime format, should be yyyy-mm-ddTHH:MM:SS-HH:MM')
+                record.datetime = dt
+            record.longitude = args.get('longitude', record.longitude)
+            record.latitude = args.get('latitude',record.latitude)
+            record.elevation = args.get('elevation', record.elevation)
+            response = 'successfully updated record {}'.format(record_id)
     return response
 
 
